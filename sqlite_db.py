1fd04d5692f6c1baa1cc549252d9893580ea1b75
Luanna Guajardo <gluannna@gmail.com>
2026-04-12 04:06:49 -0300

    Conexion a SQLite

---
+++ sqlite_db.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+class Conexion():
+    def __init__(self):
+        try:
+            self.con= sqlite3.connect("inventario.db")
+            self.crearTablas()
+        except Exception as ex: 
+            print("Error al conectar: ",ex)
+    
+    def crearTablas(self):
+        cur = self.con.cursor()
+        # Tabla de usuarios (para el login y roles)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            nombre TEXT NOT NULL, 
+            usuario TEXT UNIQUE NOT NULL,
+            clave TEXT NOT NULL,
+            rol TEXT NOT NULL DEFAULT 'vendedor' -- 'admin' o 'vendedor'
+            )""")
+
+        # Tabla categorias (para organizar los productos)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS categorias (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        nombre TEXT NOT NULL
+        )
+        """)
+
+        #Tabla productos 
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS productos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            codigo TEXT UNIQUE NOT NULL, 
+            nombre TEXT NOT NULL, 
+            id_categoria INTEGER,
+            precio REAL NOT NULL,
+            stock_actual INTEGER DEFAULT 0,
+            FOREIGN KEY (id_categoria) REFERENCES categorias(id)
+            )
+        """)
+
+        #Tabla de Ventas 
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS ventas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            id_usuario INTEGER,
+            total REAL,
+            FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+            )
+            """)    
+        
+        #Detalle de la venta (que productos se llevaron en cada venta)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS detalle_venta (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            id_venta INTEGER, 
+            id_producto INTEGER,
+            cantidad INTEGER,
+            precio_unitario REAL,
+            FOREIGN KEY (id_venta) REFERENCES ventas(id),
+            FOREIGN KEY (id_producto) REFERENCES productos(id)
+        )
+        """)
+        self.con.commit()
+
+    def cerrar(self):
+        self.con.close()
+
+#probar 
+con = Conexion()
+con.cerrar()
+
