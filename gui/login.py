39c08c5ab6b4cc80138960d713b7cf2857b62caf
martinabatista <martibatista2005@gmail.com>
2026-04-20 20:30:33 -0300

    correji el login.py, al .gitignotr lr agregue el archivo inventario.db

--- gui/login.py
+++ gui/login.py
@@ -1,27 +1,29 @@
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QMessageBox
-from PySide6.QtCore import QFile 
 from data.usuario_data import UsuarioData
+from gui.principal import Principal
+import os
 
 class Login():
     def __init__(self):
         loader = QUiLoader()
-        self.login = loader.load("gui/login.ui")
-        self.usuario_data = UsuarioData() #instancia de acceso a
+        ruta = os.path.join(os.path.dirname(__file__), "login.ui")
+        self.login = loader.load(ruta)
+
+        self.usuario_data = UsuarioData()
         self.initGUI()
-        self.login.lblError.setText("")#muestra la interfaz grafica
+        self.login.lblError.setText("")
         self.login.show()
         
     def ingresar(self):
         usuario = self.login.txtUsuario.text()
         clave = self.login.txtClave.text()
 
-        #validar los campos vacios 
         if usuario == "" and clave == "":
             self.login.lblError.setText("Complete todos los campos")
             return
 
-        user = self.usuario_data.login(usuario,clave)
+        user = self.usuario_data.login(usuario, clave)
 
         if user:
             self.login.lblError.setText("")
@@ -32,11 +34,15 @@ class Login():
                 print("Acceso de administrador")
             else: 
                 print("Acceso de vendedor")
-            #aca desp abrimos la ventana de inventario
-            #self.abrir_sistema()
+            
+            self.abrir_sistema(user)
+
         else: 
             self.login.lblError.setText("Usuario o contraseña incorrecto")
 
+    def abrir_sistema(self, user):
+        self.principal = Principal(user)
+        self.login.close()
+
     def initGUI(self):
         self.login.btnAcceder.clicked.connect(self.ingresar)
-
