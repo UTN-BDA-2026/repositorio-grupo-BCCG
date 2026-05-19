f46c328d48d650da3b957c3b2c4536d2989539a5
martinabatista <martibatista2005@gmail.com>
2026-05-18 21:03:39 -0300

    modifique admin.py y login.py para las conexiones entre las interfaces del login y admin

--- gui/admin.py
+++ gui/admin.py
@@ -4,15 +4,59 @@ import os
 class Admin():
     def __init__(self, usuario):
         loader = QUiLoader()
+
+        # cargar archivo admin.ui
         ruta = os.path.join(os.path.dirname(__file__), "admin.ui")
         self.ventana = loader.load(ruta)
-
         self.usuario = usuario
         try:
             self.ventana.lblUsuario.setText(f"Admin: {usuario.nombre}")
         except:
             pass
 
+        # inici interfaz
+        self.initGUI()
         self.ventana.show()
-        
-        #luego definiremos las acciones de los botones y demas componentes de la interfaz grafica del admin
+
+    def initGUI(self):
+
+        # boton volver
+        try:
+            self.ventana.btnVolver.clicked.connect(self.volver_login)
+        except:
+            pass
+        # botones del administrador
+        try:
+            self.ventana.btnUsuarios.clicked.connect(self.abrir_usuarios)
+        except:
+            pass
+        try:
+            self.ventana.btnProductos.clicked.connect(self.abrir_productos)
+        except:
+            pass
+        try:
+            self.ventana.btnStock.clicked.connect(self.abrir_stock)
+        except:
+            pass
+        try:
+            self.ventana.btnVentas.clicked.connect(self.abrir_ventas)
+        except:
+            pass
+    # volver al login
+    def volver_login(self):
+        from gui.login import Login
+
+        self.login = Login()
+        self.ventana.close()
+
+    def abrir_usuarios(self):
+        print("Abrir gestión de usuarios")
+
+    def abrir_productos(self):
+        print("Abrir gestión de productos")
+
+    def abrir_stock(self):
+        print("Abrir carga de stock")
+
+    def abrir_ventas(self):
+        print("Abrir reportes de ventas")
--- gui/login.py
+++ gui/login.py
@@ -2,42 +2,48 @@ from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QMessageBox
 from PySide6.QtCore import QFile
 from data.usuario_data import UsuarioData
+from gui.admin import Admin
 
 class Login():
     def __init__(self):
         loader = QUiLoader()
         self.login = loader.load("gui/login.ui")
-        self.usuario_data = UsuarioData()  # instancia de acceso a
+
+        self.usuario_data = UsuarioData()
+
         self.initGUI()
-        self.login.lblError.setText("")  # muestra la interfaz grafica
+
+        self.login.lblError.setText("")
+
         self.login.show()
         
     def ingresar(self):
+
         usuario = self.login.txtUsuario.text()
         clave = self.login.txtClave.text()
 
-        # validar los campos vacios
         if usuario == "" and clave == "":
             self.login.lblError.setText("Complete todos los campos")
             return
-
         user = self.usuario_data.login(usuario, clave)
 
         if user:
             self.login.lblError.setText("")
             print("Bienvenido:", user.nombre)
             print("Rol:", user.rol)
-
+            # acceso administrador
             if user.rol == "admin":
                 print("Acceso de administrador")
+                self.admin = Admin(user)
+                self.login.close()
+            # acceso vendedor
             else:
                 print("Acceso de vendedor")
-
-            # desp abrimos la ventana de inventario
-            # self.abrir_sistema()
-
         else:
-            self.login.lblError.setText("Usuario o contraseña incorrecto")
-
+            self.login.lblError.setText(
+                "Usuario o contraseña incorrecto"
+            )
     def initGUI(self):
-        self.login.btnAcceder.clicked.connect(self.ingresar)
+        self.login.btnAcceder.clicked.connect(
+            self.ingresar
+        )
