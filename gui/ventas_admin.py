bdca7ac505d8d0a1c562d1bbbe5cec9f652dc873
CabezaFlorncia <florenciacabeza2109@gmail.com>
2026-05-19 22:13:02 -0300

    se modifico este archivo para la conexion con bajo_stock

--- gui/ventas_admin.py
+++ gui/ventas_admin.py
@@ -1,10 +1,14 @@
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
+from gui.bajo_stock import StockBajo
 import os
 
 class Ventas():
+
     def __init__(self, usuario, volver_callback=None):
+
         loader = QUiLoader()
+
         ruta = os.path.join(
             os.path.dirname(__file__),
             "ventas_admin.ui"
@@ -12,20 +16,50 @@ class Ventas():
 
         file = QFile(ruta)
         file.open(QFile.ReadOnly)
+
         self.ventana = loader.load(file)
+
         file.close()
+
         self.usuario = usuario
         self.volver_callback = volver_callback
+
         self.initGUI()
+
         self.ventana.show()
 
     def initGUI(self):
+
         try:
-            self.ventana.btnVolver.clicked.connect(self.volver)
+            self.ventana.btnVolver.clicked.connect(
+                self.volver
+            )
+        except:
+            pass
+
+        try:
+            self.ventana.btnStockBajo.clicked.connect(
+                self.abrir_stock_bajo
+            )
         except:
             pass
 
     def volver(self):
+
         self.ventana.close()
+
         if self.volver_callback:
             self.volver_callback()
+
+    def abrir_stock_bajo(self):
+
+        self.stock = StockBajo(
+            self.usuario,
+            volver_callback=self.mostrar_ventas
+        )
+
+        self.ventana.close()
+
+    def mostrar_ventas(self):
+
+        self.ventana.show()
