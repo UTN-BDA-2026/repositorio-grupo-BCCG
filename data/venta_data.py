31b12c8556840e7dc67de0423d2fbad3c492fef2
Luanna Guajardo <gluannna@gmail.com>
2026-05-30 18:15:25 -0300

    limpiando los commits de venta_data para transaccion

--- data/venta_data.py
+++ data/venta_data.py
@@ -1,15 +1,14 @@
-from bases_de_datos.sqlite_db import Conexion
+#from bases_de_datos.sqlite_db import Conexion
 
 class VentaData():
-    def __init__(self):
-        self.db = Conexion()
+    def __init__(self,db):
+        self.db = db
 
     def crear_venta(self, id_usuario, total):
         self.db.cur.execute("""
             INSERT INTO ventas (id_usuario, total)
             VALUES (?, ?)
         """, (id_usuario, total))
-        self.db.con.commit()
         return self.db.cur.lastrowid
 
     def agregar_detalle(self, id_venta, id_producto, cantidad, precio):
@@ -17,4 +16,4 @@ class VentaData():
             INSERT INTO detalle_venta (id_venta, id_producto, cantidad, precio_unitario)
             VALUES (?, ?, ?, ?)
         """, (id_venta, id_producto, cantidad, precio))
-        self.db.con.commit()
+       
