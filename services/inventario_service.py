5b9df684fe64ac789ce1a359fbcf616cbc399b17
Luanna Guajardo <gluannna@gmail.com>
2026-05-30 18:16:16 -0300

    implemente transacciones de bd en el flujo de ventas

--- services/inventario_service.py
+++ services/inventario_service.py
@@ -1,11 +1,15 @@
 from data.producto_data import ProductoData
 from data.venta_data import VentaData
 from bases_de_datos.mongo_db import ConexionMongo
+from bases_de_datos.sqlite_db import Conexion
 
 class InventarioService():
     def __init__(self):
-        self.producto_data = ProductoData()
-        self.venta_data = VentaData()
+
+        self.db = Conexion()
+
+        self.producto_data = ProductoData(self.db)
+        self.venta_data = VentaData(self.db)
         self.mongo = ConexionMongo()
 
     def vender_producto(self, id_usuario, id_producto, cantidad):
@@ -19,26 +23,38 @@ class InventarioService():
             print("Stock insuficiente")
             return
         total = producto.precio * cantidad
-        #crear venta
-        id_venta = self.venta_data.crear_venta(id_usuario, total)
-        #guardar detalle
-        self.venta_data.agregar_detalle(
-            id_venta,
-            id_producto,
-            cantidad,
-            producto.precio
-        )
-        #actualizar stock
-        nuevo_stock = producto.stock_actual - cantidad
-        self.producto_data.actualizar_stock(id_producto, nuevo_stock)
-        #registrar en Mongo
-        self.mongo.registrar_movimiento({
-            "id_producto": id_producto,
-            "tipo": "SALIDA",
-            "cantidad": cantidad,
-            "motivo": "Venta"
-        })
-        print("Venta realizada correctamente")
+        
+        
+        try:
+            #iniciar transaccion 
+            self.db.con.execute("BEGIN TRANSACTION")
+
+             #crear venta
+            id_venta = self.venta_data.crear_venta(id_usuario, total)
+            
+            #guardar detalle
+            self.venta_data.agregar_detalle(id_venta,id_producto,cantidad,producto.precio)
+           
+            #actualizar stock
+            nuevo_stock = producto.stock_actual - cantidad
+            self.producto_data.actualizar_stock(id_producto, nuevo_stock)
+            
+            #confirma cambios en sql
+            self.db.con.commit()
+            
+            #registrar logs/movimientos en Mongo
+            self.mongo.registrar_movimiento({
+                "id_producto": id_producto,
+                "tipo": "SALIDA",
+                "cantidad": cantidad,
+                "motivo": "Venta"
+            })
+            print("Venta realizada correctamente")
+        
+        except Exception as ex:
+            #deshacer cambios
+            self.db.con.rollback()
+            print("Error en la venta:", ex)
     
     #para que el administrador pueda cargar stock
     def agregar_stock(self, id_producto, cantidad):
@@ -47,15 +63,27 @@ class InventarioService():
         if producto is None:
             print("Producto no existe")
             return
-        nuevo_stock = producto.stock_actual + cantidad
-        self.producto_data.actualizar_stock(
-            id_producto,
-            nuevo_stock
-        )
-        self.mongo.registrar_movimiento({
-            "id_producto": id_producto,
-            "tipo": "ENTRADA",
-            "cantidad": cantidad,
-            "motivo": "Reposicion"
-        })
-        print("Stock agregado correctamente")
+        
+        try: 
+            self.db.con.execute("BEGIN")
+
+            nuevo_stock = producto.stock_actual + cantidad
+            self.producto_data.actualizar_stock(
+                id_producto,
+                nuevo_stock
+            )
+
+            self.db.con.commit()
+
+            self.mongo.registrar_movimiento({
+                "id_producto": id_producto,
+                "tipo": "ENTRADA",
+                "cantidad": cantidad,
+                "motivo": "Reposicion"
+            })
+            print("Stock agregado correctamente")
+        
+        except Exception as ex:
+            self.db.con.rollback()
+
+            print("Error al agregar stock:", ex)
