444b98dd4c5a943f4a9b7a42f8075ddb9f5de754
CabezaFlorncia <florenciacabeza2109@gmail.com>
2026-06-16 17:23:58 -0300

    modifique login para la conexion con el panel de vendedor

--- gui/login.py
+++ gui/login.py
@@ -3,9 +3,11 @@ from PySide6.QtWidgets import QMessageBox
 from PySide6.QtCore import QFile
 from data.usuario_data import UsuarioData
 from gui.admin import Admin
+from gui.vendedor import Vendedor
 
 class Login():
     def __init__(self):
+
         loader = QUiLoader()
         self.login = loader.load("gui/login.ui")
 
@@ -16,36 +18,62 @@ class Login():
         self.login.lblError.setText("")
 
         self.login.show()
-        
+
     def ingresar(self):
 
         usuario = self.login.txtUsuario.text()
         clave = self.login.txtClave.text()
 
-        if usuario == "" and clave == "":
-            self.login.lblError.setText("Complete todos los campos")
+        if usuario == "" or clave == "":
+            self.login.lblError.setText(
+                "Complete todos los campos"
+            )
             return
-        user = self.usuario_data.login(usuario, clave)
+
+        user = self.usuario_data.login(
+            usuario,
+            clave
+        )
 
         if user:
+
             self.login.lblError.setText("")
+
             print("Bienvenido:", user.nombre)
             print("Rol:", user.rol)
-            # acceso administrador
+
+            # Administrador
             if user.rol == "admin":
+
                 print("Acceso de administrador")
+
                 self.admin = Admin(user)
+
                 self.login.close()
-            # acceso vendedor
-            else:
-                from gui.vendedor import Vendedor
+
+            # Vendedor
+            elif user.rol == "vendedor":
+
+                print("Acceso de vendedor")
+
                 self.vendedor = Vendedor(user)
-                self.login.close()                
+
+                self.login.close()
+
+            else:
+
+                self.login.lblError.setText(
+                    "Rol no válido"
+                )
+
         else:
+
             self.login.lblError.setText(
                 "Usuario o contraseña incorrecto"
             )
+
     def initGUI(self):
+
         self.login.btnAcceder.clicked.connect(
             self.ingresar
         )
