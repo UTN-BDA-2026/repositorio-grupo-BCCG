188ff3acb2a1fd96e5a50dd4cfb9e73c9a60accf
Luanna Guajardo <gluannna@gmail.com>
2026-06-21 04:41:33 -0300

    modifique para que muestre la hora local de la venta

--- data/venta_data.py
+++ data/venta_data.py
@@ -6,8 +6,8 @@ class VentaData():
 
     def crear_venta(self, id_usuario, total):
         self.db.cur.execute("""
-            INSERT INTO ventas (id_usuario, total)
-            VALUES (?, ?)
+            INSERT INTO ventas (id_usuario, fecha, total)
+            VALUES (?, DATETIME('now', 'localtime'),?)
         """, (id_usuario, total))
         return self.db.cur.lastrowid
 
@@ -26,3 +26,11 @@ class VentaData():
             """ , (id_usuario,))
         return self.db.cur.fetchall() #devuelve una lista de tuplas con las ventas
     
+    def obtener_detalle_venta(self, id_venta):
+        self.db.cur.execute("""
+            SELECT p.nombre, d.cantidad, d.precio_unitario, (d.cantidad * d.precio_unitario) AS subtotal
+            FROM detalle_venta d
+            JOIN productos p ON d.id_producto = p.id
+            WHERE d.id_venta = ?
+            """, (id_venta))
+        return self.db.cur.fetchall()
