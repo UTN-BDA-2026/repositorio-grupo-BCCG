e3d85bb446897658f6c91cf1eabdd6b1b8ae8703
Luanna Guajardo <gluannna@gmail.com>
2026-06-22 02:18:47 -0300

    conecte el boton del panel administrador con el  menu inventario donde muestra lo del inventario general,  gestion de stock y los productos con bajo stock

---
+++ gui/menu_inventario.py
@@ -0,0 +1,52 @@
+import os
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+from bases_de_datos.sqlite_db import Conexion   
+from gui.gestion_stock import StockAdmin
+from gui.inventario import InventarioAdmin
+from gui.bajo_stock import StockBajo
+
+class MenuInventario():
+    def __init__(self, usuario, volver_callback=None, db=None):
+        loader = QUiLoader()
+        ruta = os.path.join(os.path.dirname(__file__), "menu_inventario.ui")
+        file = QFile(ruta)
+        file.open(QFile.ReadOnly)
+        self.ventana = loader.load(file)
+        file.close()
+
+        self.usuario = usuario
+        self.volver_callback = volver_callback
+        
+        self.initGUI()
+        self.ventana.show()
+
+    def initGUI(self):
+        self.ventana.btnProductos.clicked.connect(self.abrir_inventario_general)
+        self.ventana.btnStock.clicked.connect(self.abrir_gestion_stock)
+        self.ventana.btnStockBajo.clicked.connect(self.abrir_stock_bajo)
+        self.ventana.btnVolver.clicked.connect(self.volver)
+
+    def abrir_inventario_general(self):
+        print("Abrir control de inventario")
+        self.inventario = InventarioAdmin(self.usuario, volver_callback=self.mostrar_menu_inventario, db=None)
+        self.ventana.hide()
+        self.inventario.ventana.show()
+        
+    def abrir_gestion_stock(self):
+        self.ventana.hide()
+        self.stock = StockAdmin(self.usuario, volver_callback=self.mostrar_menu_inventario, db=Conexion())
+
+    def abrir_stock_bajo(self):
+        print("Abrir reporte de stock bajo")
+        self.ventana.hide()
+        self.bajo_stock = StockBajo(self.usuario,volver_callback=self.mostrar_menu_inventario)
+
+
+    def mostrar_menu_inventario(self):
+        self.ventana.show()
+
+    def volver(self):
+        self.ventana.close()
+        if self.volver_callback:
+            self.volver_callback()
