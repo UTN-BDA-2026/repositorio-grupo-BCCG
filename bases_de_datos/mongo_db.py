5a5677e6b564923e396866d51437925a148a6140
Luanna Guajardo <gluannna@gmail.com>
2026-06-22 07:47:16 -0300

    oculte cadena de conexion de MongoDB Atlas mediante variables de entorno

--- bases_de_datos/mongo_db.py
+++ bases_de_datos/mongo_db.py
@@ -1,28 +1,31 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv() 
+
 class ConexionMongo():
     def __init__(self):
-        try:
-            self.client = MongoClient("mongodb://localhost:27017/")
+        try: 
+            mongo_uri = os.getenv("MONGO_URI")
+            
+            self.client = MongoClient(mongo_uri)
             self.db = self.client["inventario_mongo"]
-            self.coleccion = self.db["movimientos_stock"] #coleccion donde se guarda movimientos de stock
-            print("Conexion a MongoDB exitosa")
+            self.coleccion = self.db["movimientos_stock"]
+            print("Conexion a MongoDB Atlas en la nube exitosa")
         except Exception as ex:
             print("Error al conectar a MongoDB: ", ex)
     
-    def registrar_movimiento(self,data):
-        """
-        data debe ser un diccionario, ej: 
-        {'id_producto': 1, 'tipo': 'SALIDA', 'cantidad': 2, 'motivo': 'Venta'}
-        """
+    def registrar_movimiento(self, data):
         try:
             self.coleccion.insert_one(data)
             print("Movimiento registrado en Mongo")
         except Exception as ex:
             print("Error al insertar en Mongo: ", ex)
 
-# Probar la conexión
-if __name__ == "__main__":
-    mongo = ConexionMongo()
-
-# Ejemplo de prueba: registrar un movimiento
-    mongo.registrar_movimiento({'id_producto': 1, 'tipo': 'ENTRADA', 'cantidad': 10})
+    def obtener_movimientos(self):
+        try:
+            return list(self.coleccion.find())
+        except Exception as ex:
+            print("Error al obtener movimientos de Mongo: ", ex)
+            return []
